9b01aa8d5d7fcfa9034bc85fff70b5ab96d48e84
dragonspace23 <richichris@hotmail.it>
2019-04-16 13:04:04 +0200

    Definizione di clear()

--- Liste/Liste/main.cpp
+++ Liste/Liste/main.cpp
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "list.h"
+
+#ifdef _WIN32
+#define clear() system("cls")
+#endif // _WIN32
+
+#ifndef __unix__
+#define clear() system("clear")
+#endif // !__unix__
+
+
 int main(int argc, char **argv) {
 
 
